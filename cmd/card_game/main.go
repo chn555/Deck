9fffd8fb0e8aa3328c4b15aa48e99dd330bfd135
Chen Tondovski <chen.tondovski@onelayer.com>
2025-05-06 11:12:18 +0300

    cmd/card_game: new executable, added grpc server

---
+++ cmd/card_game/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log/slog"
+	"net"
+	"os"
+
+	deckImpl "github.com/chn555/card-game/internal/card_game/v1"
+	deckPb "github.com/chn555/card-game/internal/gen/proto/card_game/v1"
+	"github.com/chn555/card-game/pkg/deck"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	store := deck.NewInMemoryDeckStore()
+	deckServer, err := deckImpl.NewDeckServiceServer(store)
+	if err != nil {
+		slog.Error("Error creating deck service", err)
+		os.Exit(1)
+	}
+
+	grpcServer := grpc.NewServer()
+	deckPb.RegisterDeckServiceServer(grpcServer, deckServer)
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		slog.Error("Error creating listener", err)
+		os.Exit(1)
+	}
+
+	slog.Info("Listening on port 8080")
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		slog.Error("Error serving grpc server", err)
+		os.Exit(1)
+	}
+}
