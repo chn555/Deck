d02f3bc12c0ed43ddd209164851ecf16b10b5d77
Chen Tondovski <chen.tondovski@onelayer.com>
2025-05-11 14:21:13 +0300

    pkg/deck: persist a deck of cards
    This commit introduces the interface for persistent deck state, we are
    also including an in-memory implementation.
    
    Since implementations of the interface will likely include heavier IO
    operations or network calls, a context had been added to provide
    cancellation.
    
    A more proper solution would require a more persistent store
    (e.g., postgresql, redis) but for the simplicity of the project
    an in-memory implementation is sufficient.

---
+++ pkg/deck/store.go
@@ -0,0 +1,42 @@
+package deck
+
+import (
+	"context"
+	"fmt"
+)
+
+// Store is an abstraction of deck storage with id.
+type Store interface {
+	// Get fetches the deck for the given id.
+	Get(ctx context.Context, id string) (*Deck, error)
+	// Put stores the deck for the given id.
+	Put(ctx context.Context, id string, deck *Deck) error
+}
+
+// InMemoryDeckStore is a non-persistent implementation of Store.
+type InMemoryDeckStore struct {
+	state map[string]*Deck
+}
+
+// NewInMemoryDeckStore creates a new InMemoryDeckStore.
+func NewInMemoryDeckStore() *InMemoryDeckStore {
+	return &InMemoryDeckStore{
+		state: make(map[string]*Deck),
+	}
+}
+
+// Get fetches the deck for the given id.
+func (s *InMemoryDeckStore) Get(_ context.Context, id string) (*Deck, error) {
+	deck, ok := s.state[id]
+	if !ok {
+		return nil, fmt.Errorf("deck not found")
+	}
+
+	return deck, nil
+}
+
+// Put stores the deck for the given id.
+func (s *InMemoryDeckStore) Put(_ context.Context, id string, deck *Deck) error {
+	s.state[id] = deck
+	return nil
+}
