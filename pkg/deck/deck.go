3b959999ff1fee89dc2142c81d120f4c1bfc651b
Chen Tondovski <chen.tondovski@onelayer.com>
2025-05-11 14:28:13 +0300

    pkg/deck: fetch cards
    This commit adds logic to a deck to enable us to fetch cards
    from the top of the deck, removing the card from the deck.

--- pkg/deck/deck.go
+++ pkg/deck/deck.go
@@ -5,3 +5,26 @@ type Deck struct {
 	// Cards represents the cards in the deck.
 	Cards []Card
 }
+
+// FetchCard fetches the top card from the deck,
+// once a card has been fetched it is no longer in the deck.
+//
+// If the deck is empty, it returns an empty card and false.
+func (d *Deck) FetchCard() (Card, bool) {
+	var card Card
+	if len(d.Cards) == 0 {
+		return Card{}, false
+	}
+	if len(d.Cards) == 1 {
+		card = d.Cards[0]
+		d.Cards = make([]Card, 0)
+		return card, true
+	}
+
+	// pull the card from the end of the deck,
+	card = d.Cards[len(d.Cards)-1]
+	// And remove the last card from the deck.
+	d.Cards = d.Cards[:len(d.Cards)-1]
+
+	return card, true
+}
