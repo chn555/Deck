3a75e71e0bad3d46b1554592f13eb2d334e7dd2a
Chen Tondovski <chen.tondovski@onelayer.com>
2025-05-18 17:10:00 +0300

    pkg/deck: generate a default instance with cards
    This commit introduces a method by which we can create a deck.
    A deck includes thirteen cards of each suit, and no jokers.

--- pkg/deck/deck.go
+++ pkg/deck/deck.go
@@ -6,8 +6,48 @@ type Deck struct {
 	Cards []Card
 }
 
-// FetchCard fetches the top card from the deck,
-// once a card has been fetched it is no longer in the deck.
+// Config represents the configuration for the deck.
+type Config struct{}
+
+// NewDeck creates a new deck with the given options.
+func NewDeck() *Deck {
+	deck := newDeckWithCards()
+
+	return deck
+}
+
+// The function newDeckWithCards creates a new deck with default cards.
+//
+// The default deck is a standard 52 card deck with 4 suits and 13 values.
+func newDeckWithCards() *Deck {
+	// The numbers of cards of each suit in a standard French-suited deck.
+	// https://en.wikipedia.org/wiki/Standard_52-card_deck
+	const numberOfCardsPerSuite = 13
+
+	deck := newEmptyDeck()
+
+	// Create numberOfCardsPerSuite(13) cards for each suit.
+	for _, suit := range []Suit{Hearts, Spades, Diamonds, Clubs} {
+		for value := range numberOfCardsPerSuite {
+			// The loop will create cards with values from 0 to 12, but we want 1-13, so we
+			// increment by 1.
+			deck.Cards = append(deck.Cards, NewCard(suit, uint8(value)+1))
+		}
+	}
+
+	return deck
+}
+
+// The function newEmptyDeck creates a new empty deck.
+// It provides a valid, initialized deck, but without any cards.
+func newEmptyDeck() *Deck {
+	return &Deck{
+		Cards: make([]Card, 0),
+	}
+}
+
+// FetchCard fetches the top card from the deck, once a card has been fetched it
+// is no longer in the deck.
 //
 // If the deck is empty, it returns an empty card and false.
 func (d *Deck) FetchCard() (Card, bool) {
